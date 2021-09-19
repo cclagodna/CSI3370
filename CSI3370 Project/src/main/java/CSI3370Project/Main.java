0e4fcc43c28f5211bc8a300e36d6a3e825ce52cc
Carl L <56036586+cclagodna@users.noreply.github.com>
2021-09-19 15:09:01 -0400

    Add files via upload

---
+++ CSI3370 Project/src/main/java/CSI3370Project/Main.java
@@ -0,0 +1,16 @@
+package CSI3370Project;
+
+/**
+ *
+ * @author Carl
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+    
+}
