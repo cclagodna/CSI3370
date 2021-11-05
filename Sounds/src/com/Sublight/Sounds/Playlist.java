6d558741fe29907ff6a5620e1ca4b66b8b29a39e
rogervalade <rogervalade@oakland.edu>
2021-11-05 17:20:10 -0400

    Playlist Comment Update
    Adding comments to Playlist functions

--- Sounds/src/com/Sublight/Sounds/Playlist.java
+++ Sounds/src/com/Sublight/Sounds/Playlist.java
@@ -19,34 +19,41 @@ public class Playlist
         this.name = playlistName;
     }
 
+    // Playlist getter
     public ArrayList<Song> getPlaylist() {
         return playlist;
     }
-
+    
+    // Playlist setter (useless)?
     public void setPlaylist(ArrayList<Song> playlist) {
         this.playlist = playlist;
     }
-
+    
+    // Playlist name getter
     public String getName() {
         return name;
     }
 
+    // Playlist name setter
     public void setName(String name) {
         this.name = name;
     }
     
+    // adding songs to Playlist
     public void addSong(Song s) {
         if (!playlist.contains(s)) {
             playlist.add(s);
         }
     }
     
+    // removing songs from Playlist
     public void removeSong(Song s) {
         if (playlist.contains(s)) {
             playlist.remove(s);
         }
     }
     
+    // checking if a song is within a Playlist
     public boolean hasSong(Song s) {
         return playlist.contains(s);
     }
