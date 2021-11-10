35002be84beac6b6059daa504ea2e04b7c8972bd
Carl L <56036586+cclagodna@users.noreply.github.com>
2021-11-09 20:06:45 -0500

    Add files via upload

---
+++ SublightSounds v0.1/src/com/Sublight/Sounds/Helpers.java
@@ -0,0 +1,86 @@
+/*
+ * @author Sublight Development
+ */
+package com.Sublight.Sounds;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Helpers 
+{
+    public static String p = System.getProperty("file.separator");
+    public static String uploadSongPath = "resources" + p + "Songs" + p;
+    
+    /* Use this function when wanting to move a selected file to a target directory.
+    */
+    public static boolean uploadFile(File origFile, String newLoc) 
+    {
+        Path oldPath = Paths.get(origFile.getAbsolutePath());
+        Path newPath = Paths.get(newLoc);
+        try {
+            Files.move(oldPath, newPath, REPLACE_EXISTING);
+        } catch (IOException ex) {
+            Logger.getLogger(Helpers.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;
+    }
+    
+    // This function is used to make sure all the details of Song are included in the file upload.
+    public static String uploadCheck(Song s) 
+    {
+        File f = s.getmp3Location();
+        String sName = s.getSongName();
+        String artistName = s.getArtistName();
+        String albumName = s.getAlbumName();
+        
+        if (f.exists()) 
+        {
+            if (sName != null) 
+            {
+                if (artistName != null) 
+                {
+                    if (albumName != null) 
+                    {
+                        String filepath = uploadSongPath + sName + "+" + artistName + ".mp3";
+                        File newFileLoc = new File(filepath);
+                        Song temp = new Song(newFileLoc, sName, artistName, albumName);
+                        Helpers.uploadFile(f, filepath);
+                        temp.createJSONFile(temp);
+                        return "MP3 Uploaded Successfully!";
+                    } else {
+                        return "No Album specified!";
+                    }
+                } else {
+                    return "No Artist specified!";
+                }
+            } else {
+                return "No Song Name specified!";
+            }
+        } else {
+            return "No MP3 File to upload!";
+        }
+    }
+    
+    /* This converts file paths between Mac and Windows so the program can load
+    the songs properly despite who uploads them with the program.
+    Example: "resources/Songs/abc.mp3" becomes "resources\Songs\abc.mp3" for Windows users.
+    */
+    public static File convertFilePath(File f) 
+    {
+        String s = f.getPath();
+        char c = s.charAt(9);
+        // if the / after "resources" is not equal to your path separator, change it for the whole string.
+        if (!(s.charAt(9) == p.charAt(0))) 
+        {
+            s = s.replace(s.charAt(9), p.charAt(0));
+        }
+        return new File(s);
+    }
+    
+}
---
+++ SublightSounds v0.1/src/com/Sublight/Sounds/Song.java
@@ -0,0 +1,107 @@
+/*
+ * @author Sublight Development
+ */
+package com.Sublight.Sounds;
+
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Song 
+{
+    public static String p = System.getProperty("file.separator"); // File Separator
+   
+    private File mp3Location; // location of MP3 file for song
+    private String songName; // song name
+    private String artistName; // artist name
+    private String albumName; // album name
+    
+    public Song(File location, String song, String artist, String album) 
+    {
+        this.mp3Location = location;
+        this.songName = song;
+        this.artistName = artist;
+        this.albumName = album;
+    }
+
+    public File getmp3Location() {
+        return mp3Location;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setmp3Location(File location) {
+        this.mp3Location = location;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+    
+    // This is checking whether the song MP3 file still exists or not.
+    public boolean checkmp3FileLocation(File f) {
+        return f.exists();
+    }
+    
+    // getting the location of the JSON file
+    public static File getJSONLocation(Song s) {
+        String n = s.songName;
+        String a = s.artistName;
+        File f = new File("resources" + p + "SongJSONs" + p + n + "+" + a + ".json"); // specifying where file is stored
+        return f;
+    }
+    
+    // This creates a JSON file with the name "(NameofSong)+(NameofArtist).json"
+    public void createJSONFile(Song s)
+    {
+        File f = Song.getJSONLocation(s); // getting file location of JSON
+        Gson gson = new Gson(); // creating a GSON object
+        try (FileWriter writer = new FileWriter(f.getPath())) 
+        {
+            gson.toJson(s, writer);
+            System.out.println("Made a JSON File at " + f.getPath());
+        } catch (IOException e) {}
+        
+    }
+    
+    // This is a function that removes the MP3 from the program, and the JSON associated with it.
+    public void removeSongFiles(Song s) 
+    {
+        File mp3File = s.getmp3Location().getAbsoluteFile();
+        File jsonFile = Song.getJSONLocation(s).getAbsoluteFile();
+        String n = s.getSongName();
+        String a = s.getArtistName();
+        if (mp3File.exists()) // if the mp3 file exists, delete it
+        {
+            mp3File.delete();
+            System.out.println(n + " " + a + " MP3 file deleted.");
+        } else { // else it wasn't found
+            System.out.println(n + " " + a + " MP3 not found.");
+        }
+        if (jsonFile.exists()) { // if the JSON file exists, delete it
+            jsonFile.delete();
+            System.out.println(n + " " + a + " JSON file deleted.");
+        } else { // else it wasn't found
+            System.out.println(n + " " + a + " JSON not found.");
+        }
+    }
+}
